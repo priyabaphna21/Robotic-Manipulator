ada2dd02a180d9858920939a466b29f3077fe8e6
priyabaphna21 <59766451+priyabaphna21@users.noreply.github.com>
2022-04-07 19:50:52 +0530

    Add files via upload

---
+++ Rover/cpp practice/structure1.cpp
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <iostream>
+#include <float.h>
+using namespace std;
+struct rectangle
+{
+    int len;
+    int br;
+
+};
+
+struct complex
+{
+float real;
+float img;
+};
+
+int main()
+{
+    struct rectangle r;
+    struct img_no1;
+    img_no1.real=2;
+    img_no1.img=3;
+
+
+    r.len=5;
+    r.br=10;
+    int area = r.len*r.br;
+    cout<<
+    cout<<area<<endl;
+    
+    return 0;
+}
